12df75fe5acb86468ad3e67e2b55125ca4497d0b
carlos.cortes <carlos.cortes@pineapp.co>
2023-06-15 15:51:23 -0500

    Top enfermedades

--- src/assets/componentes/Header.jsx
+++ src/assets/componentes/Header.jsx
@@ -13,7 +13,7 @@ import * as XLSX from 'xlsx';
         <body>
           <header>
             <img src="https://cdn.discordapp.com/attachments/1118913330137022484/1118934895201353728/LogoDataA.png" alt="" />
-            <h1>Visualización de datos de salud</h1>
+            <h1>Data-Analytics SiVIGILA </h1>
           </header>
 
 
@@ -23,27 +23,19 @@ import * as XLSX from 'xlsx';
 
             <div className='select-box'>
               <select name="" className="departamentos">
-                <option value="">Bogota</option>
-                <option value="">Nariño</option>
-                <option value="">Cesar</option>
-                <option value="">Choco</option>
-                <option value="">Vaupes</option>
+                <option value="">Colombia</option>
               </select>
 
               <select name="" id="">
-                <option value="">2019</option>
-                <option value="">2020</option>
-                <option value="">2021</option>
-                <option value="">2022</option>
                 <option value="">2023</option>
               </select>
 
               <select name="" id="">
-                <option value="">Cancer Infantil</option>
                 <option value="">Dengue</option>
-                <option value="">Hepatitis</option>
-                <option value="">Chikunguya</option>
-                <option value="">Intoxicación</option>
+                <option value="">Infeccion Respiratoria</option>
+                <option value="">Malaria</option>
+                <option value="">Intento de suicidio</option>
+                <option value="">Morbilidad materna externa</option>
               </select>
             </div>
 

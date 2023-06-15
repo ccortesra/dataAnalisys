4e966f7e0977dc9d4d7e98c5e64d1c6370b8f109
carlos.cortes <carlos.cortes@pineapp.co>
2023-06-15 16:47:28 -0500

    Filtro Img

--- src/assets/componentes/Header.jsx
+++ src/assets/componentes/Header.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './Header.css'
-import * as XLSX from 'xlsx';
-
+import jsonUrl from '../componentes/pbi.json'
 
   function Header() {
     const [count, setCount] = useState(0)
     
-    
+    // Crear un diccionario vacío
+    var diccionario = {};
+
+    // Agregar elementos al diccionario
+    diccionario["Dengue"] = "https://media.discordapp.net/attachments/1118913330137022484/1119007948308496546/image.png?width=1040&height=585";
+    diccionario["clave2"] = "valor2";
+    diccionario["clave3"] = "valor3";
     return (
       <>
 
@@ -15,7 +20,10 @@ import * as XLSX from 'xlsx';
             <img src="https://cdn.discordapp.com/attachments/1118913330137022484/1118934895201353728/LogoDataA.png" alt="" />
             <h1>Data-Analytics SiVIGILA </h1>
           </header>
-
+          <div className='images-box'>
+          <img src="https://media.discordapp.net/attachments/1118913330137022484/1119006916182548592/image.png?width=1040&height=585" alt="" />
+          <img src="https://media.discordapp.net/attachments/1118913330137022484/1119006970507174069/image.png?width=1040&height=585" alt="" />
+          </div>
 
           <form id='miFormulario'>
 
@@ -30,7 +38,7 @@ import * as XLSX from 'xlsx';
                 <option value="">2023</option>
               </select>
 
-              <select name="" id="">
+              <select name="" id="enfermedad">
                 <option value="">Dengue</option>
                 <option value="">Infeccion Respiratoria</option>
                 <option value="">Malaria</option>
@@ -41,7 +49,13 @@ import * as XLSX from 'xlsx';
 
 
             <button id=''>Escoger</button>
+
+
+            <img src={diccionario["Dengue"]} alt="" />
           </form>
+          
+
+          
 
 
        
